6e9d179f4544c346aa0fe2514d6cf4518e3ffc6c
Elaine Yan <elaine.yan820619@gmail.com>
2025-10-01 19:17:07 +0800

    Update.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,10 @@
 terraform {
   required_version = ">= 1.0"
 }
-
 variable "prefix" { default = "demo" }
-
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  tags = { Name = "${ var.prefix }-vpc" }  
+  tags = { Name = "${var.prefix}-vpc" }
 }
-
 output "vpc_id" { value = aws_vpc.main.id }
