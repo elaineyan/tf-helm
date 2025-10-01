309b5676efc037f14d855ec345437f9edb1f8f6c
Elaine Yan <elaine.yan820619@gmail.com>
2025-10-01 19:35:31 +0800

    Add AWS region

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,17 @@
 terraform {
   required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "prefix" { default = "demo" }
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
