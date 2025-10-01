2005563d78a27fd5a45a89798b40e3fc3f41c052
Elaine Yan <elaine.yan820619@gmail.com>
2025-10-01 18:17:43 +0800

    Add codes.

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terrafrom {
+  required_version = ">= 1.0"
+}
+
+variable "prefix" { default = "demo" }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "${ var.prefix }-vpc" }  
+}
+
+output "vpc_id" { value = aws_vpc.main.id }
